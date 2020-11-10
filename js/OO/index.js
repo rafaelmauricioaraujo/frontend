b8c5997d60a675c99bd0a0e44052e6c42346a0ac
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-11-10 09:15:01 -0300

    classes and methods

--- js/OO/index.js
+++ js/OO/index.js
@@ -1,14 +1,30 @@
 class Client {
     name;
     id;
+}
+class Account {
     agency;
     balance;
+
+    withdrawing(amount) {
+        if (this.balance >= amount){
+            this.balance -= amount
+        }else {
+            console.log('denied');
+        }
+    }
 }
 
 const client = new Client();
 client.name = 'Araujo';
 client.id = 123;
-client.agency = 456;
-client.balance = 100;
 
 console.log(client);
+
+const account = new Account();
+account.balance = 1000;
+account.agency = 888;
+
+console.log(account);
+
+account.withdrawing(10000);
