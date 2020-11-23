f240fb6fea9b66c219e65c1160bb0b9e9b128a9f
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2020-11-23 17:18:25 -0300

    implementing interface and checking key in and instanceOf

--- js/ByteBank.js
+++ js/ByteBank.js
@@ -1,5 +1,13 @@
 export class ByteBank {
     static login(authenticable, password) {
-        return authenticable.authenticate(password);
+        if (ByteBank.isAuthenticable(authenticable)) {
+            return authenticable.authenticate(password);
+        }
+        return 'user is not authenticable';
+    }
+
+    static isAuthenticable(authenticable) {
+        return 'authenticate' in authenticable && 
+        authenticable.authenticate instanceof Function;
     }
 }
