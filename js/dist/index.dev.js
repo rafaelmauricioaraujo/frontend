5bd82d608001bedf8aabf3bcf0dc19920f4f4355
Rafael Araujo <rafael.mauricio.araujo@gmail.com>
2021-02-01 22:18:43 -0300

    javascript main text animation

---
+++ js/dist/index.dev.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var text = ['Rafael', 'Developer', 'Dog lover', 'LOTR Fan', 'Cyber r space'];
+var speed = 120;
+var index;
+var sentence = document.getElementById('sentence');
+index = sentence.innerHTML.length;
+
+function typeWriter() {
+  console.log('inner in initial: ', sentence.innerHTML);
+  console.log('index: ', index);
+
+  if (index !== 0) {
+    console.log('aqui');
+    sentence.innerHTML = sentence.innerHTML.substring(0, index - 1);
+    index--;
+    console.log('index after: ', index);
+    console.log('sentence: ', sentence.innerHTML);
+    setTimeout(typeWriter, speed);
+  }
+
+  if (sentence.innerHTML == '') {
+    if (index < text[0].length) {
+      console.log('agora aqui');
+      sentence.innerHTML += text[0].charAt(index);
+      index++;
+      console.log('index in typing: ', index);
+      setTimeout(typeWriter, speed);
+    }
+  }
+} // function typing() {
+//     if (index < text[0].length) {
+//         sentence.innerHTML += text[0].charAt(index);
+//         index++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
